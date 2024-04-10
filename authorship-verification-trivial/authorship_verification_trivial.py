da4bb17190adae5460684033f6bd9073c3346625
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-04-10 15:32:59 +0200

    cleanup example

--- authorship-verification-trivial/authorship_verification_trivial.py
+++ authorship-verification-trivial/authorship_verification_trivial.py
@@ -7,14 +7,33 @@ if __name__ == "__main__":
 
     tira = Client()
 
-    # when testing on TIRA, the dataset is automatically replaced by the test dataset
-    df = tira.pd.inputs(
+    # loading train data
+    text_train = tira.pd.inputs(
         "nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training"
     )
+    targets_train = tira.pd.truths(
+        "nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training"
+    )
+    # loading validation data (automatically replaced by test data when run on tira)
+    text_validation = tira.pd.inputs(
+        "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training"
+    )
+    targets_validation = tira.pd.truths(
+        "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training"
+    )
 
-    prediction = df.set_index("id")["text"].str.contains("delve", case=False)
-    prediction.name = "label"
+    # classifying the data
+    prediction = (
+        text_validation.set_index("id")["text"]
+        .str.contains("delve", case=False)
+        .astype(int)
+    )
+
+    # converting the prediction to the required format
+    prediction.name = "generated"
     prediction = prediction.reset_index()
+
+    # saving the prediction
     output_directory = get_output_directory(str(Path(__file__).parent))
     prediction.to_json(
         Path(output_directory) / "predictions.jsonl", orient="records", lines=True
