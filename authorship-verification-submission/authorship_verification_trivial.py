5653961eab6a5e1fad45bce0ba2fd0ad81215978
Nailia (Nellie) <nelliemirz@gmail.com>
2024-04-24 12:37:52 +0000

    test

---
+++ authorship-verification-submission/authorship_verification_trivial.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+from tira.rest_api_client import Client
+from tira.third_party_integrations import get_output_directory
+
+if __name__ == "__main__":
+
+    tira = Client()
+
+    # loading train data
+    text_train = tira.pd.inputs(
+        "nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training"
+    )
+    targets_train = tira.pd.truths(
+        "nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training"
+    )
+    # loading validation data (automatically replaced by test data when run on tira)
+    text_validation = tira.pd.inputs(
+        "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training"
+    )
+    targets_validation = tira.pd.truths(
+        "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training"
+    )
+
+    # classifying the data
+    prediction = (
+        text_validation.set_index("id")["text"]
+        .str.contains("delve", case=False)
+        .astype(int)
+    )
+
+    # converting the prediction to the required format
+    prediction.name = "generated"
+    prediction = prediction.reset_index()
+
+    # saving the prediction
+    output_directory = get_output_directory(str(Path(__file__).parent))
+    prediction.to_json(
+        Path(output_directory) / "predictions.jsonl", orient="records", lines=True
+    )
